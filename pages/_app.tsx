11e2800bf3fcef9c001e83378f9091e545914255
xyntechx <xyntechx@gmail.com>
2022-12-21 12:31:32 +0700

    ✨ Enable Vercel analytics and Supabase context

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,19 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { Analytics } from "@vercel/analytics/react";
+import { supabase } from "../utils/supabase";
+import { SessionContextProvider } from "@supabase/auth-helpers-react";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
-}
+const MyApp = ({ Component, pageProps }: AppProps) => {
+    return (
+        <SessionContextProvider
+            supabaseClient={supabase}
+            initialSession={pageProps.initialSession}
+        >
+            <Component {...pageProps} />
+            <Analytics />
+        </SessionContextProvider>
+    );
+};
+
+export default MyApp;
