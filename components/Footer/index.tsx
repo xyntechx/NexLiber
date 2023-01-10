135ff3377382c799eedb218acdc6fa251266a1b4
xyntechx <xyntechx@gmail.com>
2023-01-10 10:39:04 +0800

    ✨ New links

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,20 +1,26 @@
 import Link from "next/link";
-import styles from "./Footer.module.css"
+import styles from "./Footer.module.css";
 
 const Footer = () => {
     return (
         <footer className={styles.footer}>
             <div className={styles.div}>
-                <Link href="/library" className={styles.link}>Library</Link>
-                <Link href="/docs" className={styles.link}>Docs</Link>
+                <Link href="/library" className={styles.link}>
+                    Library
+                </Link>
                 <Link href="/community" target="_blank" className={styles.link}>
                     Discord
                 </Link>
-                <Link href="https://github.com/xyntechx/NexLiber" target="_blank" className={styles.link}>
-                    GitHub
+                <Link href="/docs" className={styles.link}>
+                    Docs
+                </Link>
+                <Link href="/terms-and-conditions" className={styles.link}>
+                    Terms and Conditions
                 </Link>
             </div>
-            <sub className={styles.sub}>Copyright &copy; 2022 Nyx Audrey Angelo Iskandar</sub>
+            <sub className={styles.sub}>
+                Copyright &copy; 2022 Nyx Audrey Angelo Iskandar
+            </sub>
         </footer>
     );
 };
