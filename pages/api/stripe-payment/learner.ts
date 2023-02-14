86b00d371ab2ade2d33703976a71b0fb61c10790
xyntechx <xyntechx@gmail.com>
2023-02-14 08:56:30 +0800

    🎉 Learner payment API

---
+++ pages/api/stripe-payment/learner.ts
@@ -0,0 +1,32 @@
+import type { NextApiRequest, NextApiResponse } from "next";
+
+type Data = {
+    url: string;
+};
+
+const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
+    const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
+    const body = JSON.parse(req.body);
+
+    const session = await stripe.checkout.sessions.create({
+        mode: "payment",
+        line_items: [
+            {
+                price: process.env.STRIPE_BUY_PREMIUM_WB_PRICE_ID,
+                quantity: 1,
+            },
+        ],
+        success_url: `${process.env.NEXT_PUBLIC_ROOT_URL}/workbook/${body.slug}`,
+        cancel_url: `${process.env.NEXT_PUBLIC_ROOT_URL}/library/error`,
+        payment_intent_data: {
+            metadata: body,
+            transfer_data: { destination: body.creator_stripe_acc_id },
+        },
+    });
+
+    res.status(200).json({
+        url: session.url,
+    });
+};
+
+export default handler;
