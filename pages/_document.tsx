42b5aad057e3b277ae3af030aa9a31c51d6bef96
xyntechx <xyntechx@gmail.com>
2023-02-16 07:29:51 +0800

    ✨ Handle new favicon

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -20,33 +20,31 @@ const MyDocument = () => {
                 <link
                     rel="apple-touch-icon"
                     sizes="180x180"
-                    href="/favicons/apple-touch-icon.png?v=3"
+                    href="/favicons/apple-touch-icon.png?v=2"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="32x32"
-                    href="/favicons/favicon-32x32.png?v=3"
+                    href="/favicons/favicon-32x32.png?v=2"
                 />
                 <link
                     rel="icon"
                     type="image/png"
                     sizes="16x16"
-                    href="/favicons/favicon-16x16.png?v=3"
+                    href="/favicons/favicon-16x16.png?v=2"
                 />
-                <link rel="manifest" href="/favicons/site.webmanifest?v=3" />
+                <link rel="manifest" href="/favicons/site.webmanifest?v=2" />
                 <link
                     rel="mask-icon"
-                    href="/favicons/safari-pinned-tab.svg?v=3"
+                    href="/favicons/safari-pinned-tab.svg?v=2"
                     color="#ff6633"
                 />
-                <link rel="shortcut icon" href="/favicons/favicon.ico?v=3" />
-                <meta name="apple-mobile-web-app-title" content="NexLiber" />
-                <meta name="application-name" content="NexLiber" />
+                <link rel="shortcut icon" href="/favicons/favicon.ico?v=2" />
                 <meta name="msapplication-TileColor" content="#da532c" />
                 <meta
                     name="msapplication-config"
-                    content="/favicons/browserconfig.xml?v=3"
+                    content="/favicons/browserconfig.xml?v=2"
                 />
                 <meta name="theme-color" content="#ff6633" />
             </Head>
