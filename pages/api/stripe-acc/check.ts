4a3b67758603880ed9cae1cb0c8cac3730469c7f
xyntechx <xyntechx@gmail.com>
2023-02-16 19:31:53 +0800

    ✨ Handle null stripeID

--- pages/api/stripe-acc/check.ts
+++ pages/api/stripe-acc/check.ts
@@ -9,11 +9,14 @@ const handler = async (req: NextApiRequest, res: NextApiResponse<Data>) => {
     const body = JSON.parse(req.body);
     const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
-    const account = await stripe.accounts.retrieve(body.stripe_id);
+    let account;
+
+    if (body.stripe_id)
+        account = await stripe.accounts.retrieve(body.stripe_id);
 
     res.status(200).json({
-        charges_enabled: account.charges_enabled,
-        details_submitted: account.details_submitted,
+        charges_enabled: body.stripe_id ? account.charges_enabled : false,
+        details_submitted: body.stripe_id ? account.details_submitted : false,
     });
 };
 
